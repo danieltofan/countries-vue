be011907a2f8f57b0e8c77c4950650dce7e512d0
Daniel Tofan <daniel.c.tofan@gmail.com>
2020-02-18 18:42:30 -0500

    Initial commit - app skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-navbar variant="faded" fixed="top">
+      <b-navbar-brand tag="h3">Where in the world?</b-navbar-brand>
+    </b-navbar>
+    <b-container fluid class="main-app">
+      <b-row>
+        <b-col lg="4" md="6" class="mb-2">
+          <b-form-input
+            type="text"
+            v-model="countryName"
+            list="country-name-list"
+            id="country-name-search"
+            placeholder="Search for a country..."
+          ></b-form-input>
+
+          <datalist id="country-name-list">
+            <option v-for="name in allCountryNames" :key="name">{{
+              name
+            }}</option>
+          </datalist>
+        </b-col>
+        <b-col lg="4" offset-lg="4" md="5" offset-md="1">
+          <b-form-select
+            v-model="filterByRegion"
+            :options="regions"
+          ></b-form-select>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { mapActions, mapState } from "vuex";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      countryName: "",
+      regions: [
+        { value: null, html: "<i>Select a geographical region</i>" },
+        { value: "Africa", text: "Africa" },
+        { value: "Americas", text: "Americas" },
+        { value: "Asia", text: "Asia" },
+        { value: "Europe", text: "Europe" },
+        { value: "Oceania", text: "Oceania" }
+      ],
+      filterByRegion: ""
+    };
+  },
+  computed: {
+    ...mapState(["allCountryNames"])
+  },
+  methods: {
+    ...mapActions(["getAllCountrieNames"])
+  },
+  mounted() {
+    this.getAllCountrieNames();
   }
 };
 </script>
@@ -23,6 +70,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: aliceblue;
+  height: calc(100vh - 60px);
+  margin: 60px auto 0;
+  padding: 40px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    allCountryNames: []
+  },
+  mutations: {
+    setCountrieNames(state, data) {
+      state.allCountryNames = data;
+    }
+  },
+  actions: {
+    async getAllCountrieNames({ commit }) {
+      let url = "https://restcountries.eu/rest/v2/all?fields=name";
+      let json = await axios.get(url);
+      commit(
+        "setCountrieNames",
+        json.data.map(item => item.name)
+      );
+    }
+  },
   modules: {}
 });
