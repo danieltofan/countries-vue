402d38085d32b350810bd100de6fbbe555832ef8
Daniel Tofan <daniel.c.tofan@gmail.com>
2020-02-28 11:33:25 -0500

    Build app to deploy on pages

---
+++ docs/precache-manifest.cb775250c17f7bcdea76875e2131c850.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe8238ceb4a892f16b6",
+    "url": "/countries-vue/css/app.46bfb0f4.css"
+  },
+  {
+    "revision": "2758ab867180e30dd7ce",
+    "url": "/countries-vue/css/chunk-4289d38b.1220fca1.css"
+  },
+  {
+    "revision": "1cfadc26086eacb39508",
+    "url": "/countries-vue/css/chunk-vendors.992a103e.css"
+  },
+  {
+    "revision": "5aa4d001b6fa582ae5a610afe26e65ae",
+    "url": "/countries-vue/index.html"
+  },
+  {
+    "revision": "2fe8238ceb4a892f16b6",
+    "url": "/countries-vue/js/app.720d292d.js"
+  },
+  {
+    "revision": "2758ab867180e30dd7ce",
+    "url": "/countries-vue/js/chunk-4289d38b.08cfd4d6.js"
+  },
+  {
+    "revision": "1cfadc26086eacb39508",
+    "url": "/countries-vue/js/chunk-vendors.756e5224.js"
+  },
+  {
+    "revision": "eaad674dbd2a1131ba56d722ba2306f1",
+    "url": "/countries-vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/countries-vue/robots.txt"
+  }
+]);
