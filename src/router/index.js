7974e1f82529bbdb6e9c2c8dda1f3085fbe69d12
Daniel Tofan <danieltofan@users.noreply.github.com>
2025-12-03 15:49:21 -0500

    fix: correct base path for countries-vue repo

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/countries/'),
+  history: createWebHistory('/countries-vue/'),
   routes
 })
 
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/countries/',
+  base: '/countries-vue/',
   test: {
     globals: true,
     environment: 'jsdom'
